7a4cad9bcd4305489029aa1541d999371525dcea
AIS <arttuilamarisalo@gmail.com>
2023-12-26 15:25:09 +0200

    final push

--- ft_printf.c
+++ ft_printf.c
@@ -6,65 +6,65 @@
 /*   By: asalo <asalo@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:24:57 by asalo             #+#    #+#             */
-/*   Updated: 2023/12/24 16:40:52 by asalo            ###   ########.fr       */
+/*   Updated: 2023/12/26 15:23:10 by asalo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_find_format(char type, va_list ap, int temp)
+int	ft_find_format(char type, va_list ap, int tmp)
 {
 	int	count;
 
 	count = 0;
 	if (type == 'c')
-		temp = ft_print_char(va_arg(ap, int));
+		tmp = ft_print_char(va_arg(ap, int));
 	else if (type == 's')
-		temp = ft_print_str(va_arg(ap, char *));
+		tmp = ft_print_str(va_arg(ap, char *));
 	else if (type == 'p')
-		temp = ft_print_ptr(va_arg(ap, unsigned long));
+		tmp = ft_print_ptr(va_arg(ap, unsigned long));
 	else if (type == 'd' || type == 'i')
-		temp = ft_print_dg((long) va_arg(ap, int), "0123456789");
+		tmp = ft_print_dg((long) va_arg(ap, int), "0123456789");
 	else if (type == 'u')
-		temp = ft_print_dg((long) va_arg(ap, unsigned int), "0123456789");
+		tmp = ft_print_dg((long) va_arg(ap, unsigned int), "0123456789");
 	else if (type == 'x')
-		temp = ft_print_dg((long) va_arg(ap, unsigned int), "0123456789abcdef");
+		tmp = ft_print_dg((long) va_arg(ap, unsigned int), "0123456789abcdef");
 	else if (type == 'X')
-		temp = ft_print_dg((long) va_arg(ap, unsigned int), "0123456789ABCDEF");
+		tmp = ft_print_dg((long) va_arg(ap, unsigned int), "0123456789ABCDEF");
 	else
-		temp = write(1, &type, 1);
-	if (temp == -1)
+		tmp = write(1, &type, 1);
+	if (tmp == -1)
 	{
 		va_end(ap);
 		return (-1);
 	}
-	return (count += temp);
+	return (count += tmp);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	int		count;
-	int		temp;
-	int		temp2;
+	int		tmp;
+	int		tmp2;
 
 	va_start(ap, format);
 	count = 0;
 	while (*format)
 	{
-		temp = 0;
-		temp2 = 0;
+		tmp = 0;
+		tmp2 = 0;
 		if (*format == '%')
-			temp = ft_find_format(*(++format), ap, temp);
+			tmp = ft_find_format(*(++format), ap, tmp);
 		else
-			temp2 = write(1, format, 1);
-		if (temp == -1 || temp2 == -1)
+			tmp2 = write(1, format, 1);
+		if (tmp == -1 || tmp2 == -1)
 		{
 			count = -1;
 			break ;
 		}
 		format++;
-		count += temp + temp2;
+		count += tmp + tmp2;
 	}
 	va_end(ap);
 	return (count);
